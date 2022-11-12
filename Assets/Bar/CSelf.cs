42668ce7a5707c351d6d2b21f5a6d6e26944e96d
sun404n <sun248n@gmail.com>
2022-11-13 02:41:51 +0900

    Update making jam.unity

---
+++ Assets/Bar/CSelf.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CSelf : MonoBehaviour
+{
+    Image SelfBar;
+    public static float MaxSelf = 100f;
+    public static float self = 0f;
+    public static float CalSe = 0f;
+
+
+    void Start()
+    {
+        CalSe = 30f;
+        Re(CalSe);
+
+    }
+    void Re(float CalSe)
+    {
+        SelfBar = GetComponent<Image>();
+        self += CalSe;//30
+        SelfBar.fillAmount = self / MaxSelf;
+    }
+
+}
---
+++ Assets/Bar/CSmart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CSmart : MonoBehaviour
+{
+    Image SmartBar;
+
+    public static float MaxSmart = 100f;
+    public static float smart = 10f;
+    public static float CalS = 0f;
+
+
+    void Start()
+    {
+        CalS = 20f;
+        Re(CalS);
+
+    }
+    void Re(float CalS)
+    {
+        SmartBar = GetComponent<Image>();
+        smart += CalS;//30
+        SmartBar.fillAmount = smart / MaxSmart;
+    }
+
+}
