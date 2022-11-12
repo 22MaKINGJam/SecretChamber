f6a5af02c1adda1ccf911b34afb294aa2a0da4ef
sun404n <sun248n@gmail.com>
2022-11-12 17:03:55 +0900

    5시

---
+++ Assets/CSelf.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CSelf : MonoBehaviour
+{
+
+    Image SelfBar;
+    float MaxSelf = 100f;
+    public static float Self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelfBar = GetComponent<Image>();
+        Self = 80f;
+        //여기에 새 변수 저장
+    }
+
+    void Update()
+    {
+        SelfBar.fillAmount = Self / MaxSelf;
+    }
+}
